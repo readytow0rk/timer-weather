c7c0e57dd05d1f5b70ba2b0c7f8fdde5fadfcb09
Nikita <you@example.com>
2023-02-07 12:58:44 +0000

    convert code from qt to regular code

--- run.py
+++ run.py
@@ -1,174 +1,94 @@
-import sys
 import time
-from PyQt6 import QtCore, QtGui, QtWidgets
-from First import Ui_First
-from Second import Ui_Timer
-from weather import Ui_weather
-from alert import Ui_reminder
+import colorama 
 from pyowm import OWM
-from alert2 import Ui_alert2
 
 
-app = QtWidgets.QApplication(sys.argv)
-
-First = QtWidgets.QDialog()
-ui = Ui_First()
-ui.setupUi(First)
-First.show()
+name = input('Timer or Weather ?\n  T or W:')
 
 
-def openTimerWindow():
-    global Timer
-    Timer = QtWidgets.QDialog()
-    ui = Ui_Timer()
-    ui.setupUi(Timer)
-    First.close()
-    Timer.show()
-
-    # work of timer...
-
-    def timerStart():
-        # while int(timeUser) < 1 or int(timeUser) > 10000:
-        #     ui.lineEdit.setText('error')
-            # name = input('Enter your name:')
-
-            # while name == '':
-            #     print('write something')
-            #     name = input('Enter your name:')
-            # print('ok') i understand how working validation but i have not enough time to find rules for this lirary to do it
-
-
-                          
-            i = 0  # hours
-            ii = 0  # minutes
-            timeUser = int(ui.lineEdit_2.text())
-            comment = ui.lineEdit.text()
-            for q in range(timeUser):
-                time.sleep(60)  # set to 60
-                i += 1
-            showAlert()
-            # ui.lineEdit.setText(ui.lineEdit.text())
-            return
+while name != 't'  :
+    print('only Timer or Weather')
+    name = input('"' + name + '"' + ' not allowed\n  T or W:' )
+if name == 't':    
+    while True:
+        i = 0 #hours
+        ii = 0 #minutes
+        timeUser = int(input('Set seconds:'))
+        comment = str(input('Write comment:'))
+        for q in range(timeUser):
+            time.sleep(1)
+            i += 1
+        print('time out')
+        print(comment)
     
-        
-# and i not understanding how to share text from timer to reminder as well
-    def timerClose():
-        Timer.close()
-
-    def showAlert():
-        global reminder
-        reminder = QtWidgets.QDialog()
-        ui = Ui_reminder()
-        ui.setupUi(reminder)
-        reminder.show()
-
-        def alertRep():
-            reminder.close()
-            Timer.show()
-
-        def alertOff():
-            reminder.close()
-
-        ui.pushButton.clicked.connect(alertRep)
-        ui.pushButton_2.clicked.connect(alertOff)
-
-    ui.pushButton.clicked.connect(timerClose)
-    ui.pushButton.clicked.connect(timerStart)
-    # work of timer...
-    def returnToMain():
-        Timer.close()
-        First.show()
-
-    ui.pushButton_2.clicked.connect(returnToMain)
 
 
-ui.pb1.clicked.connect(openTimerWindow)
+
+if name == 'w':
+    colorama.init()
+
+    print(colorama.Back.GREEN)
 
 
-def openWeather():
-    global weather
-    weather = QtWidgets.QDialog()
-    ui = Ui_weather()
-    ui.setupUi(weather)
-    First.close()
-    weather.show()
+    owm = OWM("4383a10a783aec00988fb6992379ab92")
+    mgr = owm.weather_manager()
+    place = input('Where u live?')
+    observation = mgr.weather_at_place(place)
+    w = observation.weather
 
-    def getWeather():
-      # while name == '':
-    #       print('write something')
-    #       name = input('Enter your name:')
-      # print('ok') i understand how working validation but i have not enough time to find rules for this lirary to do it
-        owm = OWM("4383a10a783aec00988fb6992379ab92")
-        mgr = owm.weather_manager()
-        place = ui.lineEdit.text()
-        observation = mgr.weather_at_place(place)
-        w = observation.weather
+    temp = w.temperature("celsius")["temp"]
 
-        temp = w.temperature("celsius")["temp"]
-
-        # while ui.lineEdit.text() == observation :
-        #   ui.weatherexport.setText('Write a COUNTRY OR CITY')
-        if temp < 5:
-            ui.weatherexport.setText(
-                "In "
-                + place
-                + " "
-                + w.detailed_status
-                + " now."
-                + " \nIt about : "
-                + str(temp)
-                + " degree."
-                + " "
-                "\nPut on all your clothes )"
-            )
-        elif temp < 10:
-            ui.weatherexport.setText(
-                "In "
-                + place
-                + " "
-                + w.detailed_status
-                + " now."
-                + " \nIt about : "
-                + str(temp)
-                + " degree."
-                + " "
-                "\n50/50 but  cold."
-            )
-        elif temp < 20:
-            ui.weatherexport.setText(
-                "In "
-                + place
-                + " "
-                + w.detailed_status
-                + " now."
-                + " \nIt about : "
-                + str(temp)
-                + " degree."
-                + " "
-                "\nput on everything you want."
-            )
-        else:
-            ui.weatherexport.setText(
-                "In "
-                + place
-                + " "
-                + w.detailed_status
-                + " now."
-                + " \nIt about : "
-                + str(temp)
-                + " degree."
-                + " "
-                "\nhell on the Eart."
-            )
-
-    ui.but1.clicked.connect(getWeather)
-
-    def returnToMainn():
-        weather.close()
-        First.show()
-
-    ui.but2.clicked.connect(returnToMainn)
-
-
-ui.pb2_2.clicked.connect(openWeather)
-sys.exit(app.exec())
+      
+    if temp < 5:
+        print(
+            "In "
+            + place
+            + " "
+            + w.detailed_status
+            + " now."
+            + " \nIt about : "
+            + str(temp)
+            + " degree."
+            + " "
+            "\nPut on all your clothes )"
+        )
+    elif temp < 10:
+        print(
+            "In "
+            + place
+            + " "
+            + w.detailed_status
+            + " now."
+            + " \nIt about : "
+            + str(temp)
+            + " degree."
+            + " "
+            "\n50/50 but  cold."
+        )
+    elif temp < 20:
+        print(
+            "In "
+            + place
+            + " "
+            + w.detailed_status
+            + " now."
+            + " \nIt about : "
+            + str(temp)
+            + " degree."
+            + " "
+            "\nput on everything you want."
+        )
+    else:
+        print(
+            "In "
+            + place
+            + " "
+            + w.detailed_status
+            + " now."
+            + " \nIt about : "
+            + str(temp)
+            + " degree."
+            + " "
+            "\nhell on the Eart."
+        )
+    input()
