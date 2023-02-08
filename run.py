0f2519ab28181a98a6b712a10990c56f4ba37870
Nikita <you@example.com>
2023-02-08 09:12:27 +0000

    import colorama and added validation to weather

--- run.py
+++ run.py
@@ -1,51 +1,56 @@
 import time
-import colorama 
+import colorama
 from pyowm import OWM
 
 colorama.init()
-        
+
 print(colorama.Back.GREEN)
 
-name = input('Timer or Weather ?\n  T or W:') 
+name = input("Timer or Weather ?\n  T or W:")
 
 
-while name != 't' or name != 'w':
-    colorama.init()   
+accepted_inputs = ["w", "t"]
+while name not in accepted_inputs:
+    colorama.init()
     print(colorama.Back.RED)
-    print('only Timer or Weather')
-    name = input('"' + name + '"' + ' not allowed\n  T or W:' )
-if name == 't':    
-    while True:
+    print("only Timer or Weather")
+    name = input('"' + name + '"' + " not allowed\n  T or W:")
+if name == "t":
+    if True:
         colorama.init()
         print(colorama.Back.YELLOW)
-        i = 0 #hours
-        ii = 0 #minutes
-        timeUser = int(input('Set seconds:'))
-        comment = str(input('Write comment:'))
+        i = 0  # hours
+        ii = 0  # minutes
+        timeUser = int(input("Set seconds:"))
+        comment = str(input("Write comment:"))
         for q in range(timeUser):
             time.sleep(1)
             i += 1
-        print('time out')
-        print(comment)
-    
+        print("time out")
+        print("Your comment is: " + comment)
+        name = input("Timer or Weather ?\n  T or W:")
+
+    accepted_inputs = ["w", "t"]
+    while name not in accepted_inputs:
+        colorama.init()
+        print(colorama.Back.RED)
+        print("only Timer or Weather")
+        name = input('"' + name + '"' + " not allowed\n  T or W:")
 
 
-
-if name == 'w':
+if name == "w":
     colorama.init()
 
     print(colorama.Back.GREEN)
 
-
     owm = OWM("4383a10a783aec00988fb6992379ab92")
     mgr = owm.weather_manager()
-    place = input('Where u live?')
+    place = input("Where u live?")
     observation = mgr.weather_at_place(place)
     w = observation.weather
 
     temp = w.temperature("celsius")["temp"]
 
-      
     if temp < 5:
         print(
             "In "
@@ -98,4 +103,11 @@ if name == 'w':
             + " "
             "\nhell on the Eart."
         )
-    input()
+    name = input("Timer or Weather ?\n  T or W:")
+
+    accepted_inputs = ["w", "t"]
+    while name not in accepted_inputs:
+        colorama.init()
+        print(colorama.Back.RED)
+        print("only Timer or Weather")
+        name = input('"' + name + '"' + " not allowed\n  T or W:")
